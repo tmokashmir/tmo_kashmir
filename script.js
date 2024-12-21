9d7728a776420357fb2248ed1259553ebeefef48
Top Models Of Kashmir <nighanighamir@gmail.com>
2024-12-21 12:03:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// JavaScript for handling form submission
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for contacting us! We will get back to you soon.');
-});
